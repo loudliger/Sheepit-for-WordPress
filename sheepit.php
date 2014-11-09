a3677d01e9ca9c52423b3cdb74f4d322a54f12a3
loudliger <andrew@loudliger.com>
2014-11-09 18:34:02 -0500

    First commit
    The initial plugin to build upon. Just adds the basic functionality with
    wp_enqueue_scripts and adds custom javascript to the footer in
    wp_print_footer_scripts. Works with Contact Form 7 in limited tests.

---
+++ sheepit.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name: Sheepit! for WordPress (Basic)
+ * Plugin URI: N/A
+ * Description: Testing Sheepit JQuery plugin for WordPress
+ * Version: 0.0.1
+ * Author: Andrew Fraser
+ * Author URI: http://www.loudliger.com
+ * License: GPL2
+ */
+// Register Script
+function sheepit_scripts() {
+
+	wp_register_script( 'sheepit', '/wp-content/plugins/sheepit/js/jquery.sheepItPlugin.js', array( 'jquery' ), false, false );
+	wp_enqueue_script( 'sheepit' );
+
+	wp_enqueue_script( 'custom_sheepit', '/wp-content/plugins/sheepit/js/custom.js', array( ' jquery' ), false, false);
+}
+
+// Hook into the 'wp_enqueue_scripts' action
+add_action( 'wp_enqueue_scripts', 'sheepit_scripts' );
+
+function insert_function() {
+    echo "
+<script>
+jQuery(document).ready(function() {
+     
+    var sheepItForm = jQuery('#sheepItForm').sheepIt({
+        separator: '',
+        allowRemoveLast: true,
+        allowRemoveCurrent: true,
+        allowRemoveAll: true,
+        allowAdd: true,
+        allowAddN: true,
+        maxFormsCount: 10,
+        minFormsCount: 0,
+        iniFormsCount: 2
+    });
+ 
+});
+</script>
+";
+}
+add_action('wp_print_footer_scripts', 'insert_function');
+?>
